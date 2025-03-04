bd79f6dd16d0229c2bb295d6d62c1306650faea3
ageon-server <bittest042@outlook.com>
2025-03-04 09:18:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-BOT_TOKEN = "7450391744:AAEb__pEdwLYY4zfb7F8WZPDs12XswyEUD8"
-OPENAI_API_KEY = "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"
-OWNER_ID = 6552242136  # Replace with your Telegram ID
-OWNER_USERNAME = "@AGEON_OWNER"
+import os
+
+ class config:
+BOT_TOKEN = os.getenv "7450391744:AAEb__pEdwLYY4zfb7F8WZPDs12XswyEUD8"
+OPENAI_API_KEY = os.getenv "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"
+OWNER_ID = os.getenv '6552242136'  # Replace with your Telegram ID
+OWNER_USERNAME = os.getenv "@AGEON_OWNER"
